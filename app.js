1360509718f3c541496af875cfb94a6fed664ece
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-06-08 19:55:38 +0545

    some changes in important data

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //require is used to import modules in node.js
 const express = require('express') //express is a web application framework for Node.js, designed for building web applications and APIs. It provides a robust set of features for web and mobile applications, making it easier to manage routes, handle requests and responses, and integrate with various middleware.
+const connectToDatabase = require('./database/db')
+connectToDatabase()
 const app = express()
-const mongoose=require('mongoose')
+
+
 //app.get is used to get data from the server and send to client.it takes two paramerters, first is the route '/' which is the root route means where route means the path of url example localhost:3000 is using root route and second is a callback function that will be executed when a get request is made to specified route.
 app.get("/",(req,res)=>{
     //this is api route , eha / vaneko api ho so when we hit / in url then this api will be called and callback function will be executed
