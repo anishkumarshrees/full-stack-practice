85f8c16f4fcc2f34287317b8295f7b69a9b8fa0b
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-06-08 13:12:10 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express') //express is a web application framework for Node.js, designed for building web applications and APIs. It provides a robust set of features for web and mobile applications, making it easier to manage routes, handle requests and responses, and integrate with various middleware.
+const app = express()
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
+
+
+
+ 
