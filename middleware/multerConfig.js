e4d50ebb67b4714a412d3495be3be3cefdf67409
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-06-10 20:01:50 +0545

    add multer so we can upload images also

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
         cb(null,'./storage') //yo chai file kaha store garne vanne ho so yo chai storage folder ma store garne vanne ho eg: cb(erro,success) so error vaneko chai null ho ra success vaneko chai storage folder ho
     },
     filename:function(req,file,cb){
-            cb(null, Date.now() +  file.originalname)
+            cb(null, Date.now() +"-"+  file.originalname)
     }//filename vaneko chai file ko name tetikai rakni ki change garni vanne ho so esko kam modify garni ki tetikai rakhni vanni ho
     //esko main kaam chai jastai file ko name eautai 2 ota user le upload garyo vani details haru modify garera rakhxa
 })
