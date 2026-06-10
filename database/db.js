1e030fefa251fbb4413b50c8b05c43995d9df06c
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-06-10 14:14:57 +0545

    gitignoe

--- database/db.js
+++ database/db.js
@@ -11,9 +11,7 @@
 
 
 //jaba databse,api request, network sanga sambandhit code xa await use garna parxa so tyo code lai async function ma rakhna parxa await le chai asynchronous code lai synchronous jasto banauna madat garcha jaba database sanga sambandhit code xa await use garna parxa so tyo code lai async function ma rakhna parxa await le chai asynchronous code lai synchronous jasto banauna madat garcha. async chai function lai banaunai parxa.
-async function connectToDatabase(){
-   await mongoose.connect(process.env.MONGODB_URL)
-}
+
 
 const mongoose=require('mongoose')
 //jaba databse,api request, network sanga sambandhit code xa await use garna parxa so tyo code lai async function ma rakhna parxa await le chai asynchronous code lai synchronous jasto banauna madat garcha jaba database sanga sambandhit code xa await use garna parxa so tyo code lai async function ma rakhna parxa await le chai asynchronous code lai synchronous jasto banauna madat garcha. async chai function lai banaunai parxa.
